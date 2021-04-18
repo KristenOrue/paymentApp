67b14691f8c9116ac93d4e9e5de7a50356be38f3
KristenOrue <oruek@oregonstate.edu>
2021-04-17 22:26:32 -0700

    Embed Card form to the Purchase fields

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,21 @@ $(document).on("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
+
+document.addEventListener("turbolinks:load", () => {
+  const public_key = document.querySelector("meta[name='stripe-key']").getAttribute("content")
+  const stripe = Stripe(public_key)
+
+  const elements = stripe.elements()
+  const card = elements.create('card')
+  card.mount('#card-element')
+
+  card.addEventListener("change", (event) => {
+    var displayError = document.getElementById('card-errors')
+    if(event.error) {
+      displayError.textContent = event.error.message
+    } else {
+      displayError.textContent = ''
+    }
+  })
+})
